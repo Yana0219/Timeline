d6ea2e62e1f8a0ab1957dd84fd8bb0f67f6df140
Yana0219 <Yana0219@users.noreply.github.com>
2021-12-05 00:46:58 +0500

    footer

--- js/main.js
+++ js/main.js
@@ -2,5 +2,35 @@ $(document).ready(function () {
 
     $('.parallax-window').parallax({imageSrc: 'images/bg3.jpg'});
 
+    let years = $('.single-timeline-area .single-timeline-content');
+    console.log(years);
+    let news =  $('.news__year');
+
+    $($(years)[0]).click(function () {
+        $($(news)[0]).fadeIn('slow');
+        $($(news)[1]).fadeOut('slow');
+        $($(news)[2]).fadeOut('slow');
+        $($(news)[3]).fadeOut('slow');
+    })
+    $($(years)[1]).click(function () {
+        $($(news)[1]).fadeIn('slow');
+        $($(news)[0]).fadeOut('slow');
+        $($(news)[2]).fadeOut('slow');
+        $($(news)[3]).fadeOut('slow');
+    })
+    $($(years)[2]).click(function () {
+        $($(news)[2]).fadeIn('slow');
+        $($(news)[0]).fadeOut('slow');
+        $($(news)[1]).fadeOut('slow');
+        $($(news)[3]).fadeOut('slow');
+    })
+    $($(years)[3]).click(function () {
+        $($(news)[3]).fadeIn('slow');
+        $($(news)[0]).fadeOut('slow');
+        $($(news)[2]).fadeOut('slow');
+        $($(news)[1]).fadeOut('slow');
+    })
+
+
 })
 
